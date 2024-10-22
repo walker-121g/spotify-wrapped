2d8e91706946ca843873ccd31a32e24ad099a356
Sam Yauger <syauger@121g.io>
2024-10-22 08:44:19 -0400

    allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "2340wrapped.fly.dev",
+    "localhost",
+    "localhost:5173",
+    "localhost:8000",
+]
 
 
 # Application definition
