c9e7074095cd8c17ae7f30012a8a1cb76b017d3d
Nathan Xie <nathankeystone@hotmail.com>
2024-10-29 23:38:41 -0400

    Implemented MSW for API mocking
    Implemented Mock Service Worker (MSW) to intercept API calls and respond with mocked responces.

--- frontend/src/tests/example.test.tsx
+++ frontend/src/tests/example.test.tsx
@@ -1,5 +1,10 @@
-import { describe, test, expect } from "vitest";
+import { beforeAll, describe, test, expectTypeOf, expect, afterAll, afterEach } from "vitest";
 import { render } from "@testing-library/react";
+import {graphql, http as mswHttp, HttpResponse } from "msw";
+import { setupServer } from 'msw/node'
+import { UserContext } from "@/stores/user.store";
+import { http } from '../services/http.service';
+import { useAuth } from '../stores/auth.store';
 
 import {
   useQuery,
@@ -51,3 +56,68 @@ describe("<Example />", () => {
     expect(getByText("Example")).toBeTruthy();
   });
 });
+describe ("<InterceptionTest />", () => {
+  let response: Response
+  let body: UserContext
+  beforeAll(async () => {
+    response = await fetch(
+      'https://api.spotify.com/v1/me',
+    );
+    body = await response.json();
+  });
+  test("Basic Return", async () => {
+    expect(body.country).toEqual("America");
+    expect(body.email).toEqual("email");
+    expect(body.display_name).toEqual("john");
+    expect(response.status).toEqual(200);
+})
+})
+
+const userData = {
+  country: "America",
+  email: "email",
+  display_name: "john",
+  explicit_content: {
+    filter_enabled: true,
+    filter_locked: false,
+  },
+  external_urls: {
+    spotify: "spotify",
+  },
+  followers: {
+    href: "thing",
+    total: "placeholder",
+  },
+  href: "href",
+  id: "id",
+  product: "damn",
+  type: "type",
+  uri: "url",
+};
+
+export const restHandlers = [
+  mswHttp.get('https://api.spotify.com/v1/me', () => {
+    return HttpResponse.json(userData)
+  }),
+]
+
+const graphqlHandlers = [
+  graphql.query('ListPosts', () => {
+    return HttpResponse.json(
+      {
+        data: { userData },
+      },
+    )
+  }),
+]
+
+const server = setupServer(...restHandlers, ...graphqlHandlers)
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+
+//  Close server after all tests
+afterAll(() => server.close())
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers())
